d89e17a6c935ac5e73aa40ced3b9d95ff81bfcfc
Jonah Leggett <jonah.leggett@gmail.com>
2021-08-24 14:41:18 -0700

    Initial Commit

---
+++ BuddyCoinBUD.sol
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "53055e85-9594-4704-939e-ef70b62169be",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "### Welcome to the Buddy Coin (BUD) Blockchain Party! ###"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8c58ef35-8512-4a0d-958f-28e7676acd69",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
