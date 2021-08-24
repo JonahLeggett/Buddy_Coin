4e14c83836b937b66b2350de2c0a75b83811de58
Jonah Leggett <jonah.leggett@gmail.com>
2021-08-24 14:46:04 -0700

    initial token solidity code

--- BuddyCoinBUD.sol
+++ BuddyCoinBUD.sol
@@ -1,43 +1,63 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "53055e85-9594-4704-939e-ef70b62169be",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "### Welcome to the Buddy Coin (BUD) Blockchain Party! ###"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8c58ef35-8512-4a0d-958f-28e7676acd69",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
+Token:
+
+pragma solidity 0.5.5
+
+contract WeedToken {
+    
+    string public name=“WeedToken”;
+    string public symbol = “WEED”;
+    string public standard = “WEED Token v1.0”
+    string public decimals = 18;
+    string public totalSupply;
+    
+    mapping(address=> uint256) public balanceOf;
+    mapping(address=>mapping(address=>uint256)) public allowance;
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint_value
+    );
+    
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+    
+    function WeedToken (uint256_initialSupply) public{
+        balanceOf[msg,sender]= _initialSupply;
+        totalSupply = _initialSupply;
 }
+
+Sale contract:
+
+pragma solidity 0.5.5;
+
+import “./WeedToken.sol”;
+
+contract WeedCrowdsale {
+    
+    address admin;
+    WeedToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokenSold;
+    
+    event Sell(address _buyer, uint256 _amount);
+    
+    function WeedCrowdsale(WeedToken _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+    
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z=x*y)/ y == x);
+    }
+    
+    function buyTokens (uint x,uint y) internal pure returns (uint z) {
+        require (msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balance(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        tokenSold +- _numberOfTokens;
+    }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Buddy_Coin
 Token with smart contracts built on the Ethereum blockchain that enables easy crypto payment for cannabis companies. 
+
+### Welcome to the Buddy Coin (BUD) Blockchain Party! ###
+
