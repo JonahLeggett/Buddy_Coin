5966251b061730ee0f6222a3a23b10fce9630448
Jonah Leggett <jonah.leggett@gmail.com>
2021-08-24 19:12:19 -0700

    update crowdsale

--- BuddyToken.sol
+++ BuddyToken.sol
@@ -1,22 +1,15 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.5;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract BuddyToken is ERC20, ERC20Detailed {
-    address payable owner;
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "You do not have permission to mint these tokens!");
-        _;
-    }
-
-    constructor(uint initial_supply) ERC20Detailed("BuddyToken", "BUD", 18) public {
-        owner = msg.sender;
-        _mint(owner, initial_supply);
-    }
-
-    function mint(address recipient, uint amount) public onlyOwner {
-        _mint(recipient, amount);
-    }
+contract BuddyToken is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor(
+        string memory name,
+        string memory symbol
+    )
+        ERC20Detailed(name, symbol, 18)
+        public {
+        }
 }
--- BuddyTokenCrowdsale.sol
+++ BuddyTokenCrowdsale.sol
@@ -1,18 +1,35 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.5;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "./BuddyToken.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-contract BuddyToken is ERC20, ERC20Detailed, ERC20Mintable {
+contract BuddyTokenCrowdsale is Crowdsale, MintedCrowdsale {
+    constructor(
+        uint rate,
+        address payable wallet,
+        BuddyToken token
+        )
+        Crowdsale(rate, wallet, token) public {
+        }
+}
+    
+contract BuddyTokenCrowdsaleDeployer {
+
+    address public buddy_token_address;
+    address public buddy_crowdsale_address;
+
     constructor(
         string memory name,
         string memory symbol,
-        uint initial_supply
+        address payable wallet
     )
-        ERC20Detailed(name, symbol, 18)
-        public
-    {
-        mint(msg.sender, initial_supply);
-    }
-}
+        public {
+        BuddyToken token = new BuddyToken(name, symbol, 0);
+        buddy_token_address = address(token);
+        BuddyTokenCrowdsale buddy_crowdsale = new BuddyTokenCrowdsale(1, wallet, token);
+        buddy_crowdsale_address = address(buddy_crowdsale);
+        token.addMinter(buddy_crowdsale_address);
+        token.renounceMinter();    
+        }
+}  
